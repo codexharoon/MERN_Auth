86daae22021fd1671a98eb14c4334a6342037b29
Muhammad Haroon <theharoonsocial@gmail.com>
2024-01-26 21:26:09 +0500

    created auth signup + middle ware to handle errors

--- server/index.js
+++ server/index.js
@@ -1,15 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import db from "./DB.js";
-
-dotenv.config();
-const app = express();
+import authRoute from "./routes/auth.js";
 const PORT = 8888;
+
+const app = express();
+app.use(express.json());
+dotenv.config();
 
 db();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
+});
+
+app.use("/api/auth", authRoute);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
 
 app.listen(process.env.PORT || PORT, () => {
