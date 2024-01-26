36b10906c4d54446f9eeafb439448cc1efd50b0a
Muhammad Haroon <theharoonsocial@gmail.com>
2024-01-26 16:48:51 +0500

    create pages and added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
-import React from "react";
-
+import { Home, About, Profile, Login, Signup } from "./pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
