660bc9b93296536c79f4f2bdcd0e25c3a1a392a3
Muhammad Haroon <theharoonsocial@gmail.com>
2024-01-26 18:37:28 +0500

    setup mongodb

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+import db from "./DB.js";
+
+dotenv.config();
 const app = express();
 const PORT = 8888;
+
+db();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
