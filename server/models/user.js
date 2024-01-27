dccd1ab3d8decb016d41edd99d606ca621d2b7f6
Muhammad Haroon <theharoonsocial@gmail.com>
2024-01-27 23:44:32 +0500

    implemented google auth at server side

--- server/models/user.js
+++ server/models/user.js
@@ -16,6 +16,11 @@ const userSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    photo: {
+      type: String,
+      default:
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fprofile&psig=AOvVaw2zbLPT4Ba0v5mswU2L1b2y&ust=1706465828139000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCLDx3uGW_oMDFQAAAAAdAAAAABAD",
+    },
   },
   { timestamps: true }
 );
