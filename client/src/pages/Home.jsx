e7eb8bab46a1ca8b22f7e01c067906cd1f30a6f8
Muhammad Haroon <theharoonsocial@gmail.com>
2024-01-29 14:23:28 +0500

    improve meta data at index.html file

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,6 +6,18 @@ const Home = () => {
       <h1 className="text-3xl font-bold text-center mt-5">
         Welcome to <span className="text-red-400">Code x Auth</span>!
       </h1>
+      <p className="text-center text-sm font-bold mt-3">
+        A{" "}
+        <a
+          className="underline text-red-400 hover:text-red-500"
+          href="https://codexharoon.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          codexharoon
+        </a>{" "}
+        Project!
+      </p>
     </div>
   );
 };
